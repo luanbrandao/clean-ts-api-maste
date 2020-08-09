602cd29f1f75e286f1a45ab0c3d8a12989410c46
luanbrandao <luanbrandao4@gmail.com>
2020-08-09 11:00:09 -0300

    fix: configs jest

--- jest.config.js
+++ jest.config.js
@@ -15,10 +15,16 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
