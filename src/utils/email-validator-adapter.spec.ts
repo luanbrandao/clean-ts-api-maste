2c170a84428ea8f15a520ee2efb27975351d383f
luanbrandao <luanbrandao4@gmail.com>
2020-08-13 10:34:13 -0300

    refactor: move sut creation to a factory halper method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import { EmailValidatorAdapter } from './email-validator'
 import validator from 'validator'
+import { EmailValidator } from '../apresentation/protocols/email-validator'
 
 // nos testes unitários sempre fazer um mock;
 // nunca usar a própria implementação da lib
@@ -11,9 +12,13 @@ jest.mock('validator', () => ({
   }
 }))
 
+const makeSut = (): EmailValidator => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
 
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
 
@@ -22,13 +27,13 @@ describe('EmailValidator Adapter', () => {
   })
 
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
 
   test('Should call validator with correct email', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
 
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
 
