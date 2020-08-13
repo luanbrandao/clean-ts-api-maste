e0da272afaf87899f30dab57454503d358242045
luanbrandao <luanbrandao4@gmail.com>
2020-08-13 14:12:58 -0300

    test: ensure DBAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -118,4 +118,22 @@ describe('DbAddAccount UseCase', () => {
     // a classe encrypter não pode tratar e exceção, pois ela já é tratada no controller.
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
