8c2f140dea961f39a64efc45850400b74e79e422
luanbrandao <luanbrandao4@gmail.com>
2020-08-16 12:15:50 -0300

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+import app from '../../main/config/app'
+import request from 'supertest'
+
+describe('Content Type Middleware', () => {
+  // nos testes tem que usar rotas diferentes
+  test('Should return default content type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  test('Should return xml content type when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
