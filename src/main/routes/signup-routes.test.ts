942be78d85be9f68d7ee17a0cc1defc31b9fe6c6
luanbrandao <luanbrandao4@gmail.com>
2020-08-17 08:11:39 -0300

    refactor: connect to mongodb bofore integration testes

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,8 +1,22 @@
 /* eslint-disable no-undef */
 import app from '../../main/config/app'
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
