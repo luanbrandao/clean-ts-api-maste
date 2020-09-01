e4d9e01329bef0ab32a99e0d190875ce8c50293f
luanbrandao <luanbrandao4@gmail.com>
2020-09-01 20:55:13 -0300

    refactor: move Validation to protocols folder

--- src/main/factories/signup copy/signup-validation.ts
+++ src/main/factories/signup copy/signup-validation.ts
@@ -1,15 +1,18 @@
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
 import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../../presentation/protocols/validation'
+import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
 import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
 import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
 
-export const makeLoginValidation = (): ValidationComposite => {
+export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
 
-  for (const field of ['email', 'password']) {
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldValidation(field))
   }
+
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
 
   validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
 
