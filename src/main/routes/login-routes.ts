57434fbcb4772957d42a83203d4587559242a92e
luanbrandao <luanbrandao4@gmail.com>
2020-09-03 16:19:26 -0300

    feat: add LoginFactory

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { makeSignUpCOntroller } from '../factories/signup/signup-factory'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
+
 export default (router:Router): void => {
   router.post('/signup', adaptRoute(makeSignUpCOntroller()))
 }
