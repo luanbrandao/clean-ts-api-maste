0edd3111866199797fcb781a0b2023740a2cdb27
luanbrandao <luanbrandao4@gmail.com>
2020-09-08 19:49:15 -0300

    fix: ensure AccountMongoRepository allows admin to access any route

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -102,7 +102,8 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any_email@gmail.com')
       expect(account.password).toBe('any_passwpord')
     })
-    test('Should return an account on loadByToken with role', async () => {
+
+    test('Should return an account on loadByToken with admin role', async () => {
       const sut = makeSut()
 
       await accountCollection.insertOne({
@@ -110,10 +111,44 @@ describe('Account Mongo Repository', () => {
         email: 'any_email@gmail.com',
         password: 'any_passwpord',
         accessToken: 'any_token',
-        role: 'any_role'
+        role: 'admin'
       })
 
-      const account = await sut.loadByToken('any_token', 'any_role')
+      const account = await sut.loadByToken('any_token', 'admin')
+
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@gmail.com')
+      expect(account.password).toBe('any_passwpord')
+    })
+
+    test('Should return null on loadByToken with invalid role', async () => {
+      const sut = makeSut()
+
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_passwpord',
+        accessToken: 'any_token'
+      })
+
+      const account = await sut.loadByToken('any_token', 'admin')
+      expect(account).toBeFalsy()
+    })
+
+    test('Should return an account on loadByToken with if user is admin', async () => {
+      const sut = makeSut()
+
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_passwpord',
+        accessToken: 'any_token',
+        role: 'admin'
+      })
+
+      const account = await sut.loadByToken('any_token')
 
       expect(account).toBeTruthy()
       expect(account.id).toBeTruthy()
